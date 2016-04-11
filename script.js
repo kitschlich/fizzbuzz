da7c3151a24c097886ece0bd72da8567a8a8d0c1
Shandra Iannucci <shandrai@gmail.com>
2016-04-11 15:08:45 -0700

    Add prompt functionality

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
-	function fizzBuzz() {
-		for (var i = 1; i <= 100; i++) {
-			var value;
-			if ((i % 3 === 0) && (i % 5 === 0)) {
-				value = 'FizzBuzz';
-			} else if (i % 3 === 0) {
-				value = 'Fizz';
-			} else if (i % 5 === 0) {
-				value = 'Buzz';
-			} else {
-				value = i;
+
+	function fizzBuzz(countTo) {
+		var countTo = prompt('Count to: ');
+		if (countTo % 1 != 0) {
+			$('#numList').append('<li>Please input an integer.</li>');
+		} else {
+			for (var i = 1; i <= parseInt(countTo); i++) {
+				var value;
+				if ((i % 3 === 0) && (i % 5 === 0)) {
+					value = 'FizzBuzz';
+				} else if (i % 3 === 0) {
+					value = 'Fizz';
+				} else if (i % 5 === 0) {
+					value = 'Buzz';
+				} else {
+					value = i;
+				}
+				$('#numList').append('<li>' + value + '</li>');
 			}
-			$('#numList').append('<li>' + value + '</li>');
 		}
-	}
+	}	
 	fizzBuzz();
 });
